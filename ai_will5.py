47c2b76669181f7a3180bad02490aefb49c96c43
hardfish-max <A.m901206@gmail.com>
2025-06-07 18:07:19 +0800

    Update ai_will5.py

--- ai_will5.py
+++ ai_will5.py
@@ -1,7 +1,7 @@
-from streamlit_lottie import st_lottie
+from streamlit_lottie import st_lottie #動畫
 import requests
 import base64 #匯入圖片、音檔
-
+import streamlit as st
 
 # 取得 GROQ API 金鑰（從 Streamlit Secrets 介面匯入）
 GROQ_API_KEY = st.secrets["GROQ_API_KEY"]
