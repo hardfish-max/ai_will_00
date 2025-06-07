be61e820845f7fc5ce00ad1c715cfa83f51bc840
hardfish-max <A.m901206@gmail.com>
2025-06-08 00:37:31 +0800

    Update ai_will5.py

--- ai_will5.py
+++ ai_will5.py
@@ -138,11 +138,11 @@ st.markdown(
     }}
     </style>
    
-    <div class="audio-player">
-        <audio autoplay loop controls>
-            <source src="data:audio/mp3;base64,{audio_base64}" type="audio/mp3">
-        </audio>
-    </div>
+   # <div class="audio-player">
+    #    <audio autoplay loop controls>
+    #        <source src="data:audio/mp3;base64,{audio_base64}" type="audio/mp3">
+    #    </audio>
+   # </div>
     
    #####
    
